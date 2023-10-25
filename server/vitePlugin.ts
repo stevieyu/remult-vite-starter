2e39f46c597a1a2489b60f3ac303c1eb2b9d37c3
Stevie <yzm629@sina.cn>
2023-10-25 18:47:25 +0800

    -

--- server/vitePlugin.ts
+++ server/vitePlugin.ts
@@ -5,54 +5,54 @@ import http from "http";
 import {readFileSync} from "fs";
 
 
+let api = null;
+
+const middleware = async (
+    req: http.IncomingMessage,
+    res: http.ServerResponse,
+    next: Function
+) => {
+    if (!/^\/api/i.test(req.url)) return next()
+
+    if(!api) {
+        api = createRemultServer(options, {
+            buildGenericRequestInfo: (r) => r,
+            getRequestBody: (req) => new Promise(resolve => {
+                let data = ''
+                req.on('data', chunk => {
+                    data += chunk;
+                });
+                req.on('end', () => {
+                    resolve(JSON.parse(data));
+                });
+            }),
+        })
+    }
+
+    if (/openApi.json$/.test(req.url)) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        return res.end(JSON.stringify(api.openApiDoc(openApiDocOpts)))
+    }
+
+    if (/docs$/.test(req.url)) {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+        return res.end(readFileSync('server/swagger.html').toString())
+    }
+
+    Object.assign(res, {
+        json: (data) => {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(data))
+        },
+    });
+    const ret = await api.handle(req, res)
+    if (!ret) next()
+}
+
 interface pluginOption {
 }
 
 export default function (userOptions: Partial<pluginOption> = {}) {
-    let api = null;
-
-    const middleware = async (
-        req: http.IncomingMessage,
-        res: http.ServerResponse,
-        next: Function
-    ) => {
-        if (!/^\/api/i.test(req.url)) return next()
-
-        if(!api) {
-            api = createRemultServer(options, {
-                buildGenericRequestInfo: (r) => r,
-                getRequestBody: (req) => new Promise(resolve => {
-                    let data = ''
-                    req.on('data', chunk => {
-                        data += chunk;
-                    });
-                    req.on('end', () => {
-                        resolve(JSON.parse(data));
-                    });
-                }),
-            })
-        }
-
-        if (/openApi.json$/.test(req.url)) {
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            return res.end(JSON.stringify(api.openApiDoc(openApiDocOpts)))
-        }
-
-        if (/docs$/.test(req.url)) {
-            res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-            return res.end(readFileSync('server/swagger.html').toString())
-        }
-
-        Object.assign(res, {
-            json: (data) => {
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.end(JSON.stringify(data))
-            },
-        });
-        const ret = await api.handle(req, res)
-        if (!ret) next()
-    }
-
     return <Plugin>{
         name: 'server',
         configureServer(server) {
